81cb2664057cef6429cfdb51474a553101697499
motomichi <motomichi.motomichi@gmail.com>
2021-10-04 16:19:50 +0900

    yarn create next-app --typescriptによって生成されていた.eslintrc.jsonの内容をyarn eslint --initで生成した.eslintrc.jsに統合

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
-{
-  "extends": "next/core-web-vitals"
-}
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "next/core-web-vitals"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+};
